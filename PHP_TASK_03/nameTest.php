5c2848b86bc101d99341198321fc76db9663718a
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-07-24 23:31:08 +0600

    nameTest

--- PHP_TASK_03/nameTest.php
+++ PHP_TASK_03/nameTest.php
@@ -1,43 +1,40 @@
+<?php
+$err ="";
+$name ="";
+
+  if (isset($_GET['name']))
+  {
+    $name = $_GET['name'];
+    if (empty($name)){
+            $err= "cannot be empty";
+    }
+    if (!preg_match("/^[a-zA-Z ]*$/", $name)){
+            $err= "invalid characters";
+    }
+    if (strlen($name) < 2){
+            $err= "invalid";
+    }
+}
+  if($err=="")
+  {
+    echo $name."<br>";
+  }
+  else
+  {
+    echo "invalid";
+  }
+
+?>
+
 <html>
 <head>
+  <title></title>
 </head>
-<body>  
-
-<?php
-$nameErr = $name = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  }else {
-    $name = test_input($_POST["name"] );
-    if (!preg_match("/^[a-zA-Z ]*$/",$name ) ){
-      $nameErr = "please start with letters";
-    }
-    if (strlen($name) < 2) {
-      $nameErr = "input more then 2 latter";
-    }
-  }
-}
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  return $data;
-}
-?>
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">  
+<body>
+  <form method="get">  
   Name: <input type="text" name="name">
-  <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
-  <input type="submit" name="submit" value="Submit">  
+  <button type="submit">Submit</button> 
 </form>
-
-<?php
-echo "Input: ";
-if($nameErr == "")
-    echo $name;
-else
-	echo "invalid";
-?>
 </body>
 </html>
