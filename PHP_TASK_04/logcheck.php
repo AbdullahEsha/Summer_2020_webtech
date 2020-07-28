7261eaf869737000707197a9c4837da265f246dd
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-07-28 22:03:24 +0600

    login

--- PHP_TASK_04/logcheck.php
+++ PHP_TASK_04/logcheck.php
@@ -3,29 +3,27 @@
 	
 	if(isset($_POST['submit'])){
 
-		$uname 		= $_POST['uname'];
-		$password 	= md5($_POST['password']);
+		$uname 		= $_POST['name'];
+		$password 	= md5($_POST['pass']);
 
 		if(empty($uname) || empty($password) ){
 			echo "null submission found!";
 		}else{
-			if(isset($_COOKIE['uname']) && isset($_COOKIE['password'])){
-				if($uname == $_COOKIE['uname'] && $password == $_COOKIE['password']){
+			if(isset($_COOKIE['name']) && isset($_COOKIE['pass'])){
+				if($uname == $_COOKIE['name'] && $password == $_COOKIE['pass']){
 					
 					setcookie('status', "OK", time()+3600, '/');
-					header('location: home.php');
+					header('location: dashBoard.php');
 
 				}else{
-					header('location: login.php?msg=invalid_username/password');
+					header('location: login.php?msg=sorry_invalid_username/password');
 				}
 			}else{
 				echo "don't have any account!";
 			}
-			
 		}	
 
 	}else{
-		//echo "invalid request";
-		header('location: login.php?msg=submitbuttonerror');
+		header('location: login.php?msg=submit_button_error');
 	}
 ?>
