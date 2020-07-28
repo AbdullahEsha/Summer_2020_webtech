deafd88d3c3d86edfbb7044b8c3f92c879ba8b34
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-07-28 08:56:02 +0600

    publicHome

---
+++ PHP_TASK_04/publicHome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <table border="1px" border="1" cellpadding="5" cellspacing="0">
+  <tr>
+    <td>Xcompany</td>
+    <td>
+      <ul>
+        <a href="home.php">Home</a>|
+        <a href="login.php">Login</a>|
+        <a href="registration.php">Registration</a>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2">Welcome to Xcompany</td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center">copyright@2017</td>
+  </tr>
+</table>
+
+</body>
+</html>
