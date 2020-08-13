e158bf525d70a56cd9ab5b0d76386a489039e6cd
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-08-13 09:34:35 +0600

    validation registration

--- MID_PROJECT/regCheck.php
+++ MID_PROJECT/regCheck.php
@@ -1,18 +1,27 @@
 <?php
+    
     if(isset($_POST['submit'])){
     	$conn = mysqli_connect('localhost', 'root', '', 'webtech');
+        $query = "INSERT INTO userInfo (name, userName, email, password, dateOfBirth, userType) VALUES ('$name', '$uname', '$email', '$password', '$dob', '$userType')";
 
         $name     = $_POST['name'];
-	    $uname    = $_POST['userName'];
-	    $userType = $_POST['userType'];
-	    $email    = $_POST['email'];
-	    $password = $_POST['pass'];
+        $uname    = $_POST['userName'];
+        $userType = $_POST['userType'];
+        $email    = $_POST['email'];
+        $password = $_POST['pass'];
         $dob      = $_POST['day']."/".$_POST['month']."/".$_POST['year'];
-    
-        $query = "INSERT INTO userInfo (name, userName, email, password, dateOfBirth, userType) VALUES ('$name', '$uname',   '$email', '$password', '$dob', '$userType')";
-        $result = mysqli_query($conn, $query);
 
-        header('location: login.php');
+        if(empty($name) || empty($uname) || empty($userType) || empty($email) || empty($password) || empty($dob))
+        {
+            echo "please submit all information".'<a href="home.php"><u>Home</a>';
+        }
+        else
+        {
+            
+            $result = mysqli_query($conn, $query);
+
+            header('location: login.php');
+        } 
     }
     else
     {
