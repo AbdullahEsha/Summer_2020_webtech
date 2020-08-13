ac998cb734b0251970a6f15bf12bbdece18c0074
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-08-13 09:55:01 +0600

    update reg validation

--- MID_PROJECT/regCheck.php
+++ MID_PROJECT/regCheck.php
@@ -1,27 +1,28 @@
 <?php
     
+    $name     = $_POST['name'];
+    $uname    = $_POST['userName'];
+    $userType = $_POST['userType'];
+    $email    = $_POST['email'];
+    $password = $_POST['pass'];
+    $dob      = $_POST['day']."/".$_POST['month']."/".$_POST['year'];
+
     if(isset($_POST['submit'])){
     	$conn = mysqli_connect('localhost', 'root', '', 'webtech');
         $query = "INSERT INTO userInfo (name, userName, email, password, dateOfBirth, userType) VALUES ('$name', '$uname', '$email', '$password', '$dob', '$userType')";
 
-        $name     = $_POST['name'];
-        $uname    = $_POST['userName'];
-        $userType = $_POST['userType'];
-        $email    = $_POST['email'];
-        $password = $_POST['pass'];
-        $dob      = $_POST['day']."/".$_POST['month']."/".$_POST['year'];
-
-        if(empty($name) || empty($uname) || empty($userType) || empty($email) || empty($password) || empty($dob))
-        {
-            echo "please submit all information".'<a href="home.php"><u>Home</a>';
-        }
-        else
-        {
+        
+    if(empty($name) || empty($uname) || empty($userType) || empty($email) || empty($password) || empty($dob))
+    {
+        echo "please submit all information".'<a href="home.php"><u>Home</a>';
+    }
+    else
+    {
             
-            $result = mysqli_query($conn, $query);
+        $result = mysqli_query($conn, $query);
 
-            header('location: login.php');
-        } 
+        header('location: login.php');
+    } 
     }
     else
     {
