6dd18c1be460a0edad1cf655f2efe3b3aa46c3ba
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2020-08-25 01:12:27 +0600

    name start

---
+++ FINAL_LAB_TASK_2/name/nameTest.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <form method="get">  
+  Name: <input type="text" name="name">
+  <br><br>
+  <input type="submit" name="submit"> 
+</form>
+</body>
+</html>
